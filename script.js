dd24ee2647829045c0d64c930f8a75c01ac43e86
Kevin Massongo <kevinmassongo196@gmail.com>
2023-10-17 22:24:30 +0100

    end of code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const searchInput = document.getElementById('input-wrapper');
 searchBtn.addEventListener('click', searchBtnFunction);
 
 function searchBtnFunction(){
-    let countryName = "India";
+    let countryName = searchInput.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     fetch(finalURL)
     .then((response) => response.json())
@@ -20,6 +20,28 @@ function searchBtnFunction(){
         result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flags-img">
         <h2>${data[0].name.common}</h2>
+        <div class='wrapper'>
+            <div class="container-capital">
+                <h4>Capital : </h4>
+                <span>${data[0].capital.join('')}</span>
+            </div>
+            <div class="container-capital">
+                <h4>Continent : </h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+            <div class="container-capital">
+                <h4>Population : </h4>
+                <span>${data[0].population}</span>
+            </div>
+            <div class="container-capital">
+                <h4>Currency : </h4>
+                <span>${Object.keys(data[0].currencies)[0]} - ${data[0].currencies[Object.keys(data[0].currencies)].name}</span>
+            </div>
+            <div class="container-capital">
+                <h4>Common languages : </h4>
+                <span>${Object.values(data[0].languages).toString().split(',').join(', ')}</span>
+            </div>
+        </div>
         `
     })
 }
